d15ef11d4c32ac849f2bbb144dbff2c403e988cd
surajstudent9112@gmail.com <surajstudent9122@gmail.com>
2024-12-29 22:50:02 +0530

    "Add signup signin routes"

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { SignedOut, SignInButton , SignedIn, UserButton} from '@clerk/nextjs'
 const Header = () => {
   return (
     <div>
-      
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+
     </div>
   )
 }
